4b643ab88df975ee5d8cadc86f25738a76acc7a5
biizheng <m15840149205@163.com>
2021-11-28 16:24:14 +0800

    2.3 校验表单输入

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,27 +4,49 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
 import tacos.domain.Ingredient;
 import tacos.domain.Ingredient.Type;
 import tacos.domain.Taco;
 
+@Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
-	private Logger log = LoggerFactory.getLogger(DesignTacoController.class);
-
 	@GetMapping
 	public String showDesignForm(Model model) {
+		prepareData(model);
+		model.addAttribute("design", new Taco());
+		log.debug(" -> design has been requested!");
+		return "design";
+	}
+
+	@PostMapping
+	public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+		if (errors.hasErrors()) {
+			prepareData(model);
+			return "design";
+		}
+		log.info("Processing design: " + design);
+		return "redirect:/orders/current";
+	}
+
+	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	}
+
+	private void prepareData(Model model) {
 		List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
 				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
 				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
@@ -35,22 +57,5 @@ public class DesignTacoController {
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
 		}
-		model.addAttribute("design", new Taco());
-		log.debug(" -> design has been requested!");
-		return "design";
 	}
-
-	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
-	}
-
-	@PostMapping
-	public String processDesign(Taco design, Errors errors, Model model) {
-		if (errors.hasErrors()) {
-			return "design";
-		}
-
-		return "redirect:/orders/current";
-	}
-
 }
